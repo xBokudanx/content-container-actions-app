6602bd2c2a0a73770e934983155e20d95fe5b226
xBokudanx <admon.sistemas.mwr@gmail.com>
2023-06-21 13:49:58 +0200

    Removiendo set-output

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 echo "Hello $1" # Guardará el contenido en la variable who-to-greet.
 time=$(date) # Guarda la salida de la fecha en la variable time.
-echo "::set-output name=time::$time" # Establece las acciones a través de sintaxis de workflow.
+echo "time=$time" >> $GITHUB_OUTPUT # Establece las acciones a través de sintaxis de workflow.
