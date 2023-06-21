d827ec1eee46e936dc23f27458b8b3066aafe595
xBokudanx <admon.sistemas.mwr@gmail.com>
2023-06-21 13:39:32 +0200

    Agregado entrypoint.sh 2

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-echo "Hello $1"   # uses the who-to-greet input variable to print "Hello [who-to-greet]" in the log file
-time=$(date)      # gets the current time and sets it as an output variable that actions running later in a job can use.
-echo "::set-output name=time::$time" # sets the action's output parameter using the workflow syntax
+echo "Hello $1" # Guardará el contenido en la variable who-to-greet
+time=$(date) # Guarda la salida de la fecha en la variable time
+echo "::set-output name=time::$time" # Establece las acciones a través de sintaxis de workflow.
