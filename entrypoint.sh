79b55a58627cb24d7d344841c596510058e7e440
xBokudanx <admon.sistemas.mwr@gmail.com>
2023-06-21 13:42:37 +0200

    added /bin/bash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Hello $1" # Guardará el contenido en la variable who-to-greet.
 time=$(date) # Guarda la salida de la fecha en la variable time.
 echo "::set-output name=time::$time" # Establece las acciones a través de sintaxis de workflow.
