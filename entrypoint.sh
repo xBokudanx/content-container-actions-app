d9873dd959345596ed5bd82d597e46cf9dfb42a6
xBokudanx <admon.sistemas.mwr@gmail.com>
2023-06-21 13:41:07 +0200

    Mode Execute to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
-echo "Hello $1" # Guardará el contenido en la variable who-to-greet
-time=$(date) # Guarda la salida de la fecha en la variable time
+echo "Hello $1" # Guardará el contenido en la variable who-to-greet.
+time=$(date) # Guarda la salida de la fecha en la variable time.
 echo "::set-output name=time::$time" # Establece las acciones a través de sintaxis de workflow.
